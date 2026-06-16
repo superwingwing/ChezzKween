b1a3125561c9ddeb681a784782c6f9916c69fab9
eampatin <emmanuelampatin37@gmail.com>
2026-06-16 15:27:11 +0800

    change the color of the button

--- frontend/src/components/layout/UploadPGNModal.vue
+++ frontend/src/components/layout/UploadPGNModal.vue
@@ -51,7 +51,7 @@ async function uploadPGN() {
 <template>
   <!-- ONE BUTTON -->
   <div style="position: relative; z-index: 9999;">
-  <v-btn color="primary" @click="open">
+  <v-btn color="#4CAF50" @click="open">
     Analyze Your Game
   </v-btn>
 </div>

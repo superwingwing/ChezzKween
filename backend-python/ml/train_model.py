53aade2486931614a6b8a83217297e8c08c42f0b
eampatin <emmanuelampatin37@gmail.com>
2026-07-10 15:46:22 +0800

    remove sapacing from trai_model.py

--- backend-python/ml/train_model.py
+++ backend-python/ml/train_model.py
@@ -1,240 +1,119 @@
-# ==========================================
-# train_model.py
-#
-# Chess Playing Style Classification
-#
-# Train Machine Learning Model
-#
-# Thesis:
-# Chess Playing Style Classification
-# ==========================================
-
 import pandas as pd
 import joblib
-
 from sklearn.model_selection import train_test_split
-
 from sklearn.ensemble import RandomForestClassifier
-
 from sklearn.metrics import (
-
     accuracy_score,
-
     classification_report,
-
     confusion_matrix
-
 )
-
 from sklearn.preprocessing import LabelEncoder
 
-
-# ==========================================
-# Dataset
-# ==========================================
-
 DATASET = "dataset/chess_dataset.csv"
-
 MODEL = "models/style_classifier.pkl"
-
 ENCODER = "models/label_encoder.pkl"
 
 
-# ==========================================
 # Load Dataset
-# ==========================================
-
 df = pd.read_csv(DATASET)
-
 print()
-
 print("--------------------------------")
-
 print("Dataset Loaded")
-
 print(df.shape)
-
 print("--------------------------------")
 
 
-# ==========================================
 # Features
-# ==========================================
 
 DROP_COLUMNS = [
-
     "event",
-
     "site",
-
     "date",
-
     "white",
-
     "black",
-
     "result",
-
     "eco",
-
     "Label"
-
 ]
 
 X = df.drop(columns=DROP_COLUMNS)
-
 y = df["Label"]
 
-
-# ==========================================
 # Encode Labels
-# ==========================================
-
 encoder = LabelEncoder()
-
 y = encoder.fit_transform(y)
 
-
-# ==========================================
 # Train Test Split
-# ==========================================
-
 X_train, X_test, y_train, y_test = train_test_split(
-
     X,
-
     y,
-
     test_size=0.20,
-
     random_state=42,
-
     stratify=y
-
 )
 
-
-# ==========================================
 # Train Model
-# ==========================================
-
 model = RandomForestClassifier(
-
     n_estimators=300,
-
     random_state=42,
-
     n_jobs=-1
-
 )
 
 model.fit(
-
     X_train,
-
     y_train
-
 )
 
-
-# ==========================================
 # Prediction
-# ==========================================
-
 prediction = model.predict(X_test)
 
-
-# ==========================================
 # Evaluation
-# ==========================================
-
 print()
-
 print("--------------------------------")
-
 print("Accuracy")
-
 print("--------------------------------")
-
 print(
-
     accuracy_score(
-
         y_test,
-
         prediction
-
     )
-
 )
 
 print()
-
 print("--------------------------------")
-
 print("Classification Report")
-
 print("--------------------------------")
-
 print(
-
     classification_report(
-
         y_test,
-
         prediction,
-
         target_names=encoder.classes_
-
     )
-
 )
 
 print()
-
 print("--------------------------------")
-
 print("Confusion Matrix")
-
 print("--------------------------------")
-
 print(
-
     confusion_matrix(
-
         y_test,
-
         prediction
-
     )
-
 )
 
-
-# ==========================================
 # Save Model
-# ==========================================
-
 joblib.dump(
-
     model,
-
     MODEL
-
 )
 
 joblib.dump(
-
     encoder,
-
     ENCODER
-
 )
 
 print()
-
 print("--------------------------------")
-
 print("Model Saved")
-
 print("--------------------------------")
